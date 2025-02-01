e35739d3a7b001746155f987d6b2afb19815c163
OutlawedDev <abdulhadiarain78@gmail.com>
2025-02-01 02:45:37 -0800

    Update 321.sh

--- 321.sh
+++ 321.sh
@@ -3,36 +3,25 @@
 main() {
     clear
     echo -e "Welcome to the MacSploit Experience!"
-    echo -e "Install Script Version 2.6 (Free Version)"
-    echo -e "License Check Bypassed. Proceeding with installation..."
+    echo -e "Install Script Version 2.6"
 
-    # Скачиваем необходимые файлы
-    echo -ne "Downloading jq and hwid tools... "
-    curl -s "https://git.raptor.fun/main/jq-macos-amd64" -o "./jq"
-    chmod +x ./jq
+    echo -e "Skipping License Check... Proceeding with Installation."
 
-    
-    echo -e "Done."
-
-    # Скачиваем последнюю версию Roblox
     echo -e "Downloading Latest Roblox..."
     [ -f ./RobloxPlayer.zip ] && rm ./RobloxPlayer.zip
     local robloxVersionInfo=$(curl -s "https://clientsettingscdn.roblox.com/v2/client-version/MacPlayer")
     local versionInfo=$(curl -s "https://git.raptor.fun/main/version.json")
     
-    local mChannel=$(echo $versionInfo | ./jq -r ".channel")
-    local version=$(echo $versionInfo | ./jq -r ".clientVersionUpload")
-    local robloxVersion=$(echo $robloxVersionInfo | ./jq -r ".clientVersionUpload")
+    local mChannel=$(echo $versionInfo | jq -r ".channel")
+    local version=$(echo $versionInfo | jq -r ".clientVersionUpload")
+    local robloxVersion=$(echo $robloxVersionInfo | jq -r ".clientVersionUpload")
     
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
+    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]; then
         curl "http://setup.rbxcdn.com/mac/$robloxVersion-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
     else
         curl "http://setup.rbxcdn.com/mac/$version-RobloxPlayer.zip" -o "./RobloxPlayer.zip"
     fi
 
-    # Установка Roblox
-    rm ./jq
     echo -n "Installing Latest Roblox... "
     [ -d "./Applications/Roblox.app" ] && rm -rf "./Applications/Roblox.app"
     [ -d "/Applications/Roblox.app" ] && rm -rf "/Applications/Roblox.app"
@@ -42,23 +31,21 @@ main() {
     rm ./RobloxPlayer.zip
     echo -e "Done."
 
-    # Скачиваем и устанавливаем MacSploit
     echo -e "Downloading MacSploit..."
     curl "https://git.raptor.fun/main/macsploit.zip" -o "./MacSploit.zip"
+
     echo -n "Installing MacSploit... "
     unzip -o -q "./MacSploit.zip"
     echo -e "Done."
 
     echo -n "Updating Dylib..."
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
+    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]; then
         curl -Os "https://git.raptor.fun/preview/macsploit.dylib"
     else
         curl -Os "https://git.raptor.fun/main/macsploit.dylib"
     fi
+    
     echo -e " Done."
-
-    # Патчинг Roblox
     echo -e "Patching Roblox..."
     mv ./macsploit.dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib"
     ./insert_dylib "/Applications/Roblox.app/Contents/MacOS/macsploit.dylib" "/Applications/Roblox.app/Contents/MacOS/RobloxPlayer" --strip-codesig --all-yes
@@ -66,20 +53,18 @@ main() {
     rm -r "/Applications/Roblox.app/Contents/MacOS/RobloxPlayerInstaller.app"
     rm ./insert_dylib
 
-    # Установка приложения MacSploit
     echo -n "Installing MacSploit App... "
     [ -d "./Applications/MacSploit.app" ] && rm -rf "./Applications/MacSploit.app"
     [ -d "/Applications/MacSploit.app" ] && rm -rf "/Applications/MacSploit.app"
     mv ./MacSploit.app /Applications/MacSploit.app
     rm ./MacSploit.zip
-
+    
     touch ~/Downloads/ms-version.json
     echo $versionInfo > ~/Downloads/ms-version.json
-    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]
-    then
-        cat <<< $(./jq '.channel = "previewb"' ~/Downloads/ms-version.json) > ~/Downloads/ms-version.json
+    if [ "$version" != "$robloxVersion" ] && [ "$mChannel" == "preview" ]; then
+        cat <<< $(jq '.channel = "previewb"' ~/Downloads/ms-version.json) > ~/Downloads/ms-version.json
     fi
-    
+
     echo -e "Done."
     echo -e "Install Complete! Developed by Nexus42!"
     exit
